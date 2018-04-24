3fe4f34f366ce590a00b34a462af5590328963ee
Jodi Darien <jodydarien@yahoo.com>
2018-04-24 21:22:16 +0000

    Second commit, untested

--- app/__init__.py
+++ app/__init__.py
@@ -2,5 +2,7 @@ from flask import Flask
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'v\xf9\xf7\x11\x13\x18\xfaMYp\xed_\xe8\xc9w\x06\x8e\xf0f\xd2\xba\xfd\x8c\xda'
+UPLOAD_FOLDER = './app/static/uploads'
+filefolder = app.config['UPLOAD_FOLDER']
 
 from app import views
